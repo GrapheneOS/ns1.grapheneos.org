798222556ef1d2704dd059bbff5ac512ff9fdab4
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-05 01:26:22 -0500

    staging

--- servers.sh
+++ servers.sh
@@ -3,8 +3,7 @@ readonly servers_ns1=(
 )
 
 readonly servers=(
-    "${servers_ns1[@]}"
-    {0..2}.ns2.grapheneos.org
+    ns1.staging.grapheneos.org
 )
 
 declare -Ar threads=(
@@ -22,6 +21,7 @@ declare -Ar addresses=(
     [0.ns2.grapheneos.org]="198.98.53.141 198.251.90.93 2605:6400:10:102e:95bc:89ef:2e7f:49bb"
     [1.ns2.grapheneos.org]="205.185.124.155 198.251.90.93 2605:6400:20:1c8f:a0c9:372d:482e:945b"
     [2.ns2.grapheneos.org]="107.189.3.168 198.251.90.93 2605:6400:30:ec25:102c:af6d:5be:1eb8"
+    [ns1.staging.grapheneos.org]="198.98.56.238 2605:6400:10:c41:de92:c534:326a:711a"
 )
 
 declare -Ar continents=(
@@ -32,6 +32,7 @@ declare -Ar continents=(
     [0.ns2.grapheneos.org]=na
     [1.ns2.grapheneos.org]=na
     [2.ns2.grapheneos.org]=eu
+    [ns1.staging.grapheneos.org]=na
 )
 
 declare -Ar countries=(
@@ -42,6 +43,7 @@ declare -Ar countries=(
     [0.ns2.grapheneos.org]=us
     [1.ns2.grapheneos.org]=us
     [2.ns2.grapheneos.org]=lu
+    [ns1.staging.grapheneos.org]=us
 )
 
 declare -Ar regions=(
@@ -52,4 +54,5 @@ declare -Ar regions=(
     [0.ns2.grapheneos.org]=ny
     [1.ns2.grapheneos.org]=ca
     [2.ns2.grapheneos.org]=--
+    [ns1.staging.grapheneos.org]=ny
 )

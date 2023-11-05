cbf279bcbbc1285489c77ffa0339bb60400c997a
Daniel Micay <daniel.micay@grapheneos.org>
2023-11-05 01:26:22 -0500

    staging

--- servers.sh
+++ servers.sh
@@ -1,8 +1,5 @@
 servers=(
-    ns1.grapheneos.org
-    0.ns2.grapheneos.org
-    1.ns2.grapheneos.org
-    2.ns2.grapheneos.org
+    ns1.staging.grapheneos.org
 )
 
 declare -A addresses=(
@@ -10,4 +7,5 @@ declare -A addresses=(
     [0.ns2.grapheneos.org]="198.98.53.141 198.251.90.93 2605:6400:10:102e:95bc:89ef:2e7f:49bb"
     [1.ns2.grapheneos.org]="205.185.124.155 198.251.90.93 2605:6400:20:1c8f:a0c9:372d:482e:945b"
     [2.ns2.grapheneos.org]="107.189.3.168 198.251.90.93 2605:6400:30:ec25:102c:af6d:5be:1eb8"
+    [ns1.staging.grapheneos.org]="198.98.56.238 2605:6400:10:c41:de92:c534:326a:711a"
 )

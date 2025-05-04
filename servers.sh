99242081e69b5e8f36f2c49828f0f5afcb6546df
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-03 22:17:50 -0400

    set up 2.ns1.grapheneos.org

--- servers.sh
+++ servers.sh
@@ -1,5 +1,6 @@
 servers=(
     0.ns1.grapheneos.org
+    2.ns1.grapheneos.org
     0.ns2.grapheneos.org
     1.ns2.grapheneos.org
     2.ns2.grapheneos.org
@@ -7,6 +8,7 @@ servers=(
 
 declare -A addresses=(
     [0.ns1.grapheneos.org]="51.161.34.158 2607:5300:205:200::eaa"
+    [2.ns1.grapheneos.org]="57.129.65.223 2001:41d0:701:1100::245b"
     [0.ns2.grapheneos.org]="198.98.53.141 198.251.90.93 2605:6400:10:102e:95bc:89ef:2e7f:49bb"
     [1.ns2.grapheneos.org]="205.185.124.155 198.251.90.93 2605:6400:20:1c8f:a0c9:372d:482e:945b"
     [2.ns2.grapheneos.org]="107.189.3.168 198.251.90.93 2605:6400:30:ec25:102c:af6d:5be:1eb8"

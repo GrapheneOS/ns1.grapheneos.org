0876761fd1747f27b3534e17f3f9a791eaf2d282
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-05 17:52:30 -0400

    add 3.ns1.grapheneos.org server

--- servers.sh
+++ servers.sh
@@ -1,6 +1,7 @@
 servers=(
     0.ns1.grapheneos.org
     2.ns1.grapheneos.org
+    3.ns1.grapheneos.org
     0.ns2.grapheneos.org
     1.ns2.grapheneos.org
     2.ns2.grapheneos.org
@@ -9,6 +10,7 @@ servers=(
 declare -A addresses=(
     [0.ns1.grapheneos.org]="51.161.34.158 185.187.152.9 2607:5300:205:200::eaa 2a05:b0c4:1::8"
     [2.ns1.grapheneos.org]="57.129.65.223 185.187.152.9 2001:41d0:701:1100::245b 2a05:b0c4:1::8"
+    [3.ns1.grapheneos.org]="15.235.197.61 185.187.152.9 2402:1f00:8000:800::3966 2a05:b0c4:1::8"
     [0.ns2.grapheneos.org]="198.98.53.141 198.251.90.93 2605:6400:10:102e:95bc:89ef:2e7f:49bb"
     [1.ns2.grapheneos.org]="205.185.124.155 198.251.90.93 2605:6400:20:1c8f:a0c9:372d:482e:945b"
     [2.ns2.grapheneos.org]="107.189.3.168 198.251.90.93 2605:6400:30:ec25:102c:af6d:5be:1eb8"

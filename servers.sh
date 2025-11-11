ffb5b60e783df1a0621615a24a59f8d711770268
Daniel Micay <daniel.micay@grapheneos.org>
2025-11-11 04:25:18 -0500

    replace remaining OVH ns1 servers with Vultr

--- servers.sh
+++ servers.sh
@@ -1,5 +1,5 @@
 readonly servers_ns1=(
-    {lax,mia,nyc,sea}.ns1.grapheneos.org
+    {fra,lax,mia,nyc,sea,sin}.ns1.grapheneos.org
 )
 
 readonly servers_ns2=(
@@ -8,23 +8,20 @@ readonly servers_ns2=(
 
 readonly servers=(
     "${servers_ns1[@]}"
-    {2,3}.ns1.grapheneos.org
     "${servers_ns2[@]}"
 )
 
 declare -Ar threads=(
-    [2.ns1.grapheneos.org]=2
-    [3.ns1.grapheneos.org]=2
 )
 
 declare -Ar addresses=(
     [ns1.staging.grapheneos.org]="198.98.56.238 2605:6400:10:c41:de92:c534:326a:711a"
-    [2.ns1.grapheneos.org]="57.129.65.223 185.187.152.9 2001:41d0:701:1100::245b 2a05:b0c4:1::8"
-    [3.ns1.grapheneos.org]="15.235.197.61 185.187.152.9 2402:1f00:8000:800::3966 2a05:b0c4:1::8"
+    [fra.ns1.grapheneos.org]="80.240.20.200 185.187.152.9 2a05:f480:1800:2acd:5400:05ff:fec2:23a1 2a05:b0c4:1::8"
     [lax.ns1.grapheneos.org]="140.82.23.46 185.187.152.9 2001:19f0:6000:2dcd:5400:05ff:fec1:97db 2a05:b0c4:1::8"
     [mia.ns1.grapheneos.org]="45.32.173.220 185.187.152.9 2001:19f0:9002:2981:5400:05ff:fec1:65c4 2a05:b0c4:1::8"
     [nyc.ns1.grapheneos.org]="66.135.8.22 185.187.152.9 2001:19f0:1000:c0d4:5400:05ff:fec1:7c21 2a05:b0c4:1::8"
     [sea.ns1.grapheneos.org]="66.42.74.68 185.187.152.9 2001:19f0:8001:0438:5400:05ff:fec1:9dbd 2a05:b0c4:1::8"
+    [sin.ns1.grapheneos.org]="139.180.154.140 185.187.152.9 2401:c080:1400:4890:5400:05ff:fec2:23b0 2a05:b0c4:1::8"
     [brn.ns2.grapheneos.org]="107.189.3.168 198.251.90.93 2605:6400:30:ec25:102c:af6d:5be:1eb8"
     [las.ns2.grapheneos.org]="205.185.124.155 198.251.90.93 2605:6400:20:1c8f:a0c9:372d:482e:945b"
     [nyc.ns2.grapheneos.org]="198.98.53.141 198.251.90.93 2605:6400:10:102e:95bc:89ef:2e7f:49bb"
@@ -32,12 +29,12 @@ declare -Ar addresses=(
 
 declare -Ar continents=(
     [ns1.staging.grapheneos.org]=na
-    [2.ns1.grapheneos.org]=eu
-    [3.ns1.grapheneos.org]=as
+    [fra.ns1.grapheneos.org]=eu
     [lax.ns1.grapheneos.org]=na
     [mia.ns1.grapheneos.org]=na
     [nyc.ns1.grapheneos.org]=na
     [sea.ns1.grapheneos.org]=na
+    [sin.ns1.grapheneos.org]=as
     [brn.ns2.grapheneos.org]=eu
     [las.ns2.grapheneos.org]=na
     [nyc.ns2.grapheneos.org]=na
@@ -45,12 +42,12 @@ declare -Ar continents=(
 
 declare -Ar countries=(
     [ns1.staging.grapheneos.org]=us
-    [2.ns1.grapheneos.org]=de
-    [3.ns1.grapheneos.org]=sg
+    [fra.ns1.grapheneos.org]=de
     [lax.ns1.grapheneos.org]=us
     [mia.ns1.grapheneos.org]=us
     [nyc.ns1.grapheneos.org]=us
     [sea.ns1.grapheneos.org]=us
+    [sin.ns1.grapheneos.org]=sg
     [brn.ns2.grapheneos.org]=ch
     [las.ns2.grapheneos.org]=us
     [nyc.ns2.grapheneos.org]=us
@@ -58,12 +55,12 @@ declare -Ar countries=(
 
 declare -Ar regions=(
     [ns1.staging.grapheneos.org]=ny
-    [2.ns1.grapheneos.org]=he
-    [3.ns1.grapheneos.org]=--
+    [fra.ns1.grapheneos.org]=he
     [lax.ns1.grapheneos.org]=ca
     [mia.ns1.grapheneos.org]=fl
     [nyc.ns1.grapheneos.org]=ny
     [sea.ns1.grapheneos.org]=wa
+    [sin.ns1.grapheneos.org]=--
     [brn.ns2.grapheneos.org]=be
     [las.ns2.grapheneos.org]=ca
     [nyc.ns2.grapheneos.org]=ny

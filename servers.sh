41628c538d082647670d33fcf4aadb5ccfb5aee3
Daniel Micay <daniel.micay@grapheneos.org>
2025-12-17 22:55:33 -0500

    remove obsolete Rage4 setup
    We replaced Rage4 with our own AS and IP space.

--- servers.sh
+++ servers.sh
@@ -18,7 +18,6 @@ declare -Ar threads=(
 
 readonly ns1_ipv4=23.149.124.1
 readonly ns1_ipv6='2602:f4d9::1 2602:f4d9:2::1'
-readonly ns1_rage4_ipv4=185.187.152.9
 
 readonly ns2_ipv4=23.149.125.1
 readonly ns2_ipv6='2602:f4d9:1::1 2602:f4d9:3::1'
@@ -26,17 +25,17 @@ readonly ns2_buyvm_ipv4=198.251.90.93
 
 declare -Ar addresses=(
     [ns1.staging.grapheneos.org]="198.98.56.238 2605:6400:10:c41:de92:c534:326a:711a 2602:f4d9:4::1"
-    [bom.ns1.grapheneos.org]="65.20.91.33 $ns1_ipv4 $ns1_rage4_ipv4 2401:c080:2400:1814:5400:05ff:fec5:d503 $ns1_ipv6"
-    [fra.ns1.grapheneos.org]="80.240.20.200 $ns1_ipv4 $ns1_rage4_ipv4 2a05:f480:1800:2acd:5400:05ff:fec2:23a1 $ns1_ipv6"
-    [lax.ns1.grapheneos.org]="140.82.23.46 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:6000:2dcd:5400:05ff:fec1:97db $ns1_ipv6"
-    [lon.ns1.grapheneos.org]="192.248.159.52 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:7400:8669:5400:05ff:fec3:f3f8 $ns1_ipv6"
-    [mia.ns1.grapheneos.org]="45.32.173.220 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:9002:2981:5400:05ff:fec1:65c4 $ns1_ipv6"
-    [nyc.ns1.grapheneos.org]="66.135.8.22 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:1000:c0d4:5400:05ff:fec1:7c21 $ns1_ipv6"
-    [sao.ns1.grapheneos.org]="216.238.118.134 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:b800:14a9:5400:05ff:feca:f136 $ns1_ipv6"
-    [sea.ns1.grapheneos.org]="66.42.74.68 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:8001:0438:5400:05ff:fec1:9dbd $ns1_ipv6"
-    [sin.ns1.grapheneos.org]="139.180.154.140 $ns1_ipv4 $ns1_rage4_ipv4 2401:c080:1400:4890:5400:05ff:fec2:23b0 $ns1_ipv6"
-    [syd.ns1.grapheneos.org]="45.63.28.82 $ns1_ipv4 $ns1_rage4_ipv4 2401:c080:1800:141d:5400:05ff:fec5:d41b $ns1_ipv6"
-    [tyo.ns1.grapheneos.org]="66.42.42.15 $ns1_ipv4 $ns1_rage4_ipv4 2001:19f0:7001:5695:5400:05ff:fec4:e394 $ns1_ipv6"
+    [bom.ns1.grapheneos.org]="65.20.91.33 $ns1_ipv4 2401:c080:2400:1814:5400:05ff:fec5:d503 $ns1_ipv6"
+    [fra.ns1.grapheneos.org]="80.240.20.200 $ns1_ipv4 2a05:f480:1800:2acd:5400:05ff:fec2:23a1 $ns1_ipv6"
+    [lax.ns1.grapheneos.org]="140.82.23.46 $ns1_ipv4 2001:19f0:6000:2dcd:5400:05ff:fec1:97db $ns1_ipv6"
+    [lon.ns1.grapheneos.org]="192.248.159.52 $ns1_ipv4 2001:19f0:7400:8669:5400:05ff:fec3:f3f8 $ns1_ipv6"
+    [mia.ns1.grapheneos.org]="45.32.173.220 $ns1_ipv4 2001:19f0:9002:2981:5400:05ff:fec1:65c4 $ns1_ipv6"
+    [nyc.ns1.grapheneos.org]="66.135.8.22 $ns1_ipv4 2001:19f0:1000:c0d4:5400:05ff:fec1:7c21 $ns1_ipv6"
+    [sao.ns1.grapheneos.org]="216.238.118.134 $ns1_ipv4 2001:19f0:b800:14a9:5400:05ff:feca:f136 $ns1_ipv6"
+    [sea.ns1.grapheneos.org]="66.42.74.68 $ns1_ipv4 2001:19f0:8001:0438:5400:05ff:fec1:9dbd $ns1_ipv6"
+    [sin.ns1.grapheneos.org]="139.180.154.140 $ns1_ipv4 2401:c080:1400:4890:5400:05ff:fec2:23b0 $ns1_ipv6"
+    [syd.ns1.grapheneos.org]="45.63.28.82 $ns1_ipv4 2401:c080:1800:141d:5400:05ff:fec5:d41b $ns1_ipv6"
+    [tyo.ns1.grapheneos.org]="66.42.42.15 $ns1_ipv4 2001:19f0:7001:5695:5400:05ff:fec4:e394 $ns1_ipv6"
     [brn.ns2.grapheneos.org]="107.189.3.168 $ns2_ipv4 $ns2_buyvm_ipv4 2605:6400:30:ec25:102c:af6d:5be:1eb8 $ns2_ipv6"
     [las.ns2.grapheneos.org]="205.185.124.155 $ns2_ipv4 $ns2_buyvm_ipv4 2605:6400:20:1c8f:a0c9:372d:482e:945b $ns2_ipv6"
     [mia.ns2.grapheneos.org]="45.61.188.113 $ns2_ipv4 $ns2_buyvm_ipv4 2605:6400:0040:ffbd:14e7:d270:fd75:600c $ns2_ipv6"
